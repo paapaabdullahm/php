e1717827995813e8aeca60b32b3e110126fcb8c4
pam79 <paapaabdullahm@gmail.com>
2020-01-04 08:00:00 +0000

    Update cli toolset

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-isCommand() {
-  for cmd in "artisan"
+isCliTool() {
+  for cmd in \
+  "artisan" \
+  "bin/console" \
+  "bin/cake" \
+  "yii" \
+  "zf"
   do
     if [ -z "${cmd#"$1"}" ]; then
       return 0
@@ -11,14 +16,20 @@ isCommand() {
   return 1
 }
 
-# check if the first argument passed in looks like a flag
+# check if the first argument passed in looks like an option flag
 if [ "$(printf %c "$1")" = '-' ]; then
   set -- /tini -- php "$@"
+
+# check if the first argument passed in looks like an args... flag
+elif [ "$(printf %c "$1")" = '--' ]; then
+  set -- /tini -- php "$@"
+
 # check if the first argument passed in is php
 elif [ "$1" = 'php' ]; then
   set -- /tini -- "$@"
-# check if the first argument passed in matches a known command
-elif isCommand "$1"; then
+
+# check if the first argument passed in matches a known framework cli tool
+elif isCliTool "$1"; then
   set -- /tini -- php "$@"
 fi
 
